ce2bbb090186350eb8b3f3f32110c035c3f697f6
Quoc-Hwng <phamhung151020@gmail.com>
2021-12-28 20:35:45 +0700

    Exceeded maximum budget

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC_J8myK6jIJL0EGcgMajRcwTFNM7FNuUk",
+    authDomain: "imageshoes-e0cdf.firebaseapp.com",
+    projectId: "imageshoes-e0cdf",
+    storageBucket: "imageshoes-e0cdf.appspot.com",
+    messagingSenderId: "984191346251",
+    appId: "1:984191346251:web:081f103911929f81b64fce",
+    measurementId: "${config.measurementId}"
+  }
 };
